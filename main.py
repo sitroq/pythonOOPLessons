3d0e2fd87469b22f41b8ba7c7ed5e479b3a9e3e6
alelsey <sitroq@yandex.ru>
2021-12-05 11:39:39 +0200

    jy

--- main.py
+++ main.py
@@ -4,16 +4,18 @@ class BanckAccount:
         self.__balance = balance
         self.__passport = passport
 
-    # def print_public_data(self):
-    #     print(self.name, self.balance, self.passport)
+    def print_public_data(self):
+        self.__print_private_data()
     def print_protected_data(self):
         print(self._name, self._balance, self._passport)
-    def print_private_data(self):
+    def __print_private_data(self):
         print(self.__name, self.__balance, self.__passport)
 
 
 account1 = BanckAccount('Bob', 100000, 123456)
-account1.print_private_data()
+account1.print_public_data()
+print(dir(account1))
+print(account1._BanckAccount__balance)
 # account1.print_protected_data()
 print(account1.__name)
 print(account1.__balance)
