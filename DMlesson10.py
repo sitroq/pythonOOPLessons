e74f049e901492e9d14ed2b6fe60c0b3956b72e5
alelsey <sitroq@yandex.ru>
2021-12-05 14:55:17 +0200

    hg

---
+++ DMlesson10.py
@@ -0,0 +1,18 @@
+class UserMail:
+    def __init__(self, login, email):
+        self.login = login
+        self.__email = email
+    def get_email(self):
+        return self.__email
+    def set_email(self, value):
+        if (value.count('@') == 1 and value.count('.') == 1):
+            self.__email = value
+        else:
+            print("Ошибочная почта")
+    email = property(fget=get_email, fset=set_email)
+k = UserMail('belosnezhka', 'prince@wait.you')
+print(k.email)  # prince@wait.you
+k.email = [1, 2, 3] # Ошибочная почта
+k.email = 'prince@still@.wait'  # Ошибочная почта
+k.email = 'prince@still.wait'
+print(k.email)  # prince@still.wait
